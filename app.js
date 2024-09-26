a6e50fc2605170abd249c3a57241564101ac43d2
yahya0morsy <ym60307@gmail.com>
2024-09-27 00:47:34 +0300

    two2 commit

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ let corsOptions = {
 };
 app.use(cors(corsOptions));
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
